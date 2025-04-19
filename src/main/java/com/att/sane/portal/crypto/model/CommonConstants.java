c6e0dd8f9c1a6286e5891acb53d13d8d44039c88
Dvir Baumel <dvir.baumel@intl.att.com>
2025-04-19 23:55:45 +0300

    add liquidity validations

--- src/main/java/com/att/sane/portal/crypto/model/CommonConstants.java
+++ src/main/java/com/att/sane/portal/crypto/model/CommonConstants.java
@@ -6,4 +6,8 @@ import lombok.experimental.UtilityClass;
 public class CommonConstants {
     public static final String BNB_URL = "https://bsc-dataseed.binance.org/";
     public static final String INFURA_URL = "https://mainnet.infura.io/v3/a3afd9ca6b3946248d5a54fa8fe4a0d1";
+    public static final String FACTORY_ADDRESS_INFURA = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+    public static final String WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+    public static final String HEXADECIMAL_ZERO = "0x0000000000000000000000000000000000000000";
+
 }
--- src/main/java/com/att/sane/portal/crypto/service/NewContractValidator.java
+++ src/main/java/com/att/sane/portal/crypto/service/NewContractValidator.java
@@ -1,6 +1,8 @@
 package com.att.sane.portal.crypto.service;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
@@ -8,16 +10,25 @@ import org.springframework.stereotype.Component;
 import org.web3j.abi.FunctionEncoder;
 import org.web3j.abi.FunctionReturnDecoder;
 import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Address;
 import org.web3j.abi.datatypes.Function;
 import org.web3j.abi.datatypes.Type;
-import org.web3j.abi.datatypes.Utf8String;
+import org.web3j.abi.datatypes.generated.Uint112;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.abi.datatypes.generated.Uint32;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.request.Transaction;
 import org.web3j.protocol.core.methods.response.EthCall;
 import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.utils.Convert;
 
+import static com.att.sane.portal.crypto.model.CommonConstants.FACTORY_ADDRESS_INFURA;
+import static com.att.sane.portal.crypto.model.CommonConstants.HEXADECIMAL_ZERO;
+import static com.att.sane.portal.crypto.model.CommonConstants.WETH_ADDRESS;
 import static org.web3j.protocol.core.DefaultBlockParameterName.LATEST;
 import static org.web3j.protocol.core.methods.request.Transaction.createEthCallTransaction;
+import static org.web3j.utils.Convert.Unit.ETHER;
 
 @Component
 @Slf4j
@@ -46,12 +57,11 @@ public class NewContractValidator {
 
         boolean functionsExist = isFunctionsExist(contractAddress, web3j);
         if(functionsExist) {
-            //todo - continue;
-            return true;
+            return isLiquidityExists(web3j, contractAddress);
         }
         else {
             log.info("Outside [isValidContract] for contract address: {} with result: {}", contractAddress, functionsExist);
-            return functionsExist;
+            return false;
         }
     }
 
@@ -71,10 +81,6 @@ public class NewContractValidator {
             log.info("new contract is failed due to total supply function doesn't exist");
             result = false;
         }
-        else if(isFunctionMissing("transfer", contractAddress, web3j)) {
-            log.info("new contract is failed due to transfer function doesn't exist");
-            result = false;
-        }
 
         log.info("Inside [isFunctionsExist] for contract address: {} with result: {}", contractAddress, result);
 
@@ -82,11 +88,50 @@ public class NewContractValidator {
     }
 
     private boolean isFunctionMissing(String functionName, String contractAddress, Web3j web3j) throws IOException {
-        Function nameFunction = new Function(functionName, List.of(), List.of(TypeReference.create(Utf8String.class)));
+        Function nameFunction = new Function(functionName, List.of(), List.of(TypeReference.create(Uint256.class)));
         String encodedName = FunctionEncoder.encode(nameFunction);
         EthCall response = web3j.ethCall(createEthCallTransaction(null, contractAddress, encodedName), LATEST).send();
         List<Type> decode = FunctionReturnDecoder.decode(response.getValue(), nameFunction.getOutputParameters());
         return decode.isEmpty();
     }
 
+    private boolean isLiquidityExists(Web3j web3j, String contractAddress) throws IOException {
+        Function getPairFunction = new Function("getPair",
+                List.of(new Address(WETH_ADDRESS), new Address(contractAddress)), List.of(TypeReference.create(Address.class)));
+
+        String encodedGetPairFunction = FunctionEncoder.encode(getPairFunction);
+        EthCall response = web3j.ethCall(
+                        createEthCallTransaction(null, FACTORY_ADDRESS_INFURA, encodedGetPairFunction), LATEST)
+                .send();
+        String value = response.getValue();
+        String pairAddressValue = "0x" + value.substring(26);
+
+        if(!pairAddressValue.equals(HEXADECIMAL_ZERO)) {
+            Function getReservesFunction = new Function("getReserves", List.of(),
+                    List.of(TypeReference.create(Uint112.class), TypeReference.create(Uint112.class), TypeReference.create(Uint32.class)));
+            String encodeGetReservesFunction = FunctionEncoder.encode(getReservesFunction);
+
+            EthCall reservesResponse = web3j.ethCall(
+                            Transaction.createEthCallTransaction(null, pairAddressValue, encodeGetReservesFunction), LATEST)
+                    .send();
+
+            List<Type> decoded = FunctionReturnDecoder.decode(reservesResponse.getValue(), getReservesFunction.getOutputParameters());
+            BigInteger reserve0 = (BigInteger) decoded.getFirst().getValue();
+            BigInteger reserve1 = (BigInteger) decoded.get(1).getValue();
+
+            Function token0Function = new Function("token0",
+                    List.of(), List.of(TypeReference.create(Address.class)));
+            String encodedToken0Function = FunctionEncoder.encode(token0Function);
+            EthCall token0Response = web3j.ethCall(
+                            Transaction.createEthCallTransaction(null, pairAddressValue, encodedToken0Function), LATEST)
+                    .send();
+
+            String token0Value = "0x" + token0Response.getValue().substring(26);
+            boolean isWbnbToken0 = token0Value.equalsIgnoreCase(WETH_ADDRESS);
+            BigDecimal wbnbReserve = Convert.fromWei(new BigDecimal(isWbnbToken0 ? reserve0 : reserve1), ETHER);
+            return wbnbReserve.compareTo(new BigDecimal("1.0")) >= 0;
+        }
+
+        return false;
+    }
 }
